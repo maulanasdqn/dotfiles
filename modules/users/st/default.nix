2774f9e03c044ff5f5c2c88e38cea190fc687cb3
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-02-11 04:39:48 +0700

    Maulana Sodiqin | Add Packages

--- modules/users/st/default.nix
+++ modules/users/st/default.nix
@@ -5,10 +5,18 @@
     (st.overrideAttrs (oldAttrs: rec {
       buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
       patches = [
-        ./patch/st-nordtheme-0.9.diff
-        ./patch/st-ligatures-0.9.diff
+        (fetchpatch {
+          url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.8.5.diff";
+          sha256 = "0mgsklws6jsrngcsy64zmr604qsdlpd5pqsa3pci7j3gn8im4zyw";
+        })
+
+        (fetchpatch {
+          url = "https://st.suckless.org/patches/ligatures/0.9/st-ligatures-scrollback-20221120-0.9.diff";
+          sha256 = "0yxp83fkmj3sral609x9vb7ym09mkz2nax1952bl2lv9r0a8s6b5";
+        })
+
       ];
-      configFile = writeText "config.h" (builtins.readFile ./config/config.h);
+      configFile = writeText "config.def.h" (builtins.readFile ./config/config.def.h);
       postPatch = "${oldAttrs.postPatch}\n cp ${configFile} ./config.def.h";
     }))
   ];
