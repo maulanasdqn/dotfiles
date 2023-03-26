65d2b5f8afb4858a066779bc5dae01e6b56f9914
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-03-26 23:19:01 +0700

    improvement: add floaterm

--- modules/users/nvim/lua/config/lazy.lua
+++ modules/users/nvim/lua/config/lazy.lua
@@ -1,18 +1,27 @@
-require('lazy-lsp').setup {
-  default_config = {
-    flags = {
-      debounce_text_changes = 150,
-    },
-  },
-  configs = {
-    lua_ls = {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    },
-  },
-}
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require("lazy-lsp").setup({
+	excluded_servers = {
+		"ccls",
+		"zk",
+		"denols",
+	},
+	default_config = {
+		flags = {
+			debounce_text_changes = 150,
+		},
+		capabilities = capabilities,
+	},
+	configs = {
+		lua_ls = {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		},
+	},
+})
